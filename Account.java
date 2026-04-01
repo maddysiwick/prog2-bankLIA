93ae4a27047ce1812541f776433dd34fa9bb4566
maddysiwick <maddy.siwick@icloud.com>
2026-03-31 22:06:19 -0400

    added the skeleton of the account class

--- Account.java
+++ Account.java
@@ -1,3 +1,46 @@
+import java.util.Date;
+
 abstract public class Account {
+    protected String accountNum;
+    protected double balance;
+    protected double monthlyFee;
+
+    public Account(String accountNum, double balance, double monthlyFee) {
+        this.accountNum = accountNum;
+        this.balance = balance;
+        this.monthlyFee = monthlyFee;
+    }
+
+    abstract public void withdraw(double amount);
+
+    public void deposit(double amount){
+
+    }
+    public void deposit(double amount,String currency){
+
+    }
+    public void transfer(double amount,String accoountNumber){
+        
+    }
+
+    public String getAccountNum() {
+        return accountNum;
+    }
+    public void setAccountNum(String accountNum) {
+        this.accountNum = accountNum;
+    }
+    public double getBalance() {
+        return balance;
+    }
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+    public double getMonthlyFee() {
+        return monthlyFee;
+    }
+    public void setMonthlyFee(double monthlyFee) {
+        this.monthlyFee = monthlyFee;
+    } 
+
     
 }
